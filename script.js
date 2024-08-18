cdbfb3e339b74f1a78ef7373153acfa1834c069f
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-08-18 23:22:16 +0300

    added mobile v index page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Инициализация всех слайдеров с классом '.swiper-container'
 document.querySelectorAll('.swiper-container').forEach((slider) => {
     new Swiper(slider, {
-        slidesPerView: 3, // Количество отображаемых слайдов
+        slidesPerView: 4, // Количество отображаемых слайдов
         spaceBetween: 16, // Расстояние между слайдами
         navigation: {
             nextEl: slider.querySelector('.swiper-button-next'),
@@ -9,17 +9,29 @@ document.querySelectorAll('.swiper-container').forEach((slider) => {
         },
         breakpoints: {
             320: {
-                slidesPerView: 1,
+                slidesPerView: 1.45,
+                spaceBetween: 15,
             },
             640: {
                 slidesPerView: 2,
             },
             768: {
-                slidesPerView: 3,
+                slidesPerView: 2,
+                spaceBetween: 12,
             },
             1024: {
                 slidesPerView: 4,
             }
         }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.getElementById('burger');
+    const menu = document.getElementById('menu');
+
+    burger.addEventListener('click', function() {
+        menu.classList.toggle('active');
+        burger.classList.toggle('open');
     });
 });
